e1a1806359ca554796597d17e4fef3edb1865ae8
Alfredo Saavedra <alfredo@simplemoviles.com>
2021-11-15 16:37:45 -0300

    Seccion 8, 110 Complet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-rounting.module';
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
+
+import { AppRoutingModule } from './app-rounting.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +15,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     PaisModule,
     SharedModule
   ],
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,12 +9,25 @@ import { Component } from '@angular/core';
 })
 export class PorPaisComponent {
 
-  termino: string = '';
+  termino   : string = '';
+  hayError  : boolean = false;
+  paises    : Country[] = [];
 
-  constructor() { }
+  constructor( private PaisService: PaisService ) { }
 
-  buscar() {
-    console.log(this.termino);
+  buscar( termino: string ) {
+    this.hayError = false;
+    this.termino = termino;
+
+    this.PaisService.buscarPais( termino )
+      .subscribe( (paises) => {
+        console.log(paises);
+        this.paises = paises;
+
+      }, (err) => {
+        this.hayError = true;
+        this.paises = [];
+      });
   }
 
 }
